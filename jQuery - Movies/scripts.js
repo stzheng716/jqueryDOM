00c06ae59625759125275efd5410170549af6d43
Crynowecer0 <crynowecer11@gmail.com>
2023-04-10 20:50:54 -0400

    complete basic req + 2 further study for movies, start on sort

--- jQuery - Movies/scripts.js
+++ jQuery - Movies/scripts.js
@@ -1,14 +1,33 @@
 //use eventpreventdefault in the event listener
 
-let unorderList = $('ul')
+let unorderList = $("ul");
 
-$('form').on('submit', getValueAndappendDOM)
+$("form").on("submit", getValueAndappendDOM);
+$("ul").on("click", "button", deleteMovie);
 
-function getValueAndappendDOM(evt){
-    evt.preventDefault()
-    const titleText = $('#title').val();
-    const ratingNum = $('#rating').val();
+function getValueAndappendDOM(evt) {
+  evt.preventDefault();
+  const titleText = $("#title").val();
+  const ratingNum = $("#rating").val();
 
-    let newLI = $("<li>").text(`${titleText}: Rating ${ratingNum}`)
-    unorderList.append(newLI)
+  //build the button -> going to need to use event delegation
+  const deleteButton = $("<button>", { text: "Delete" });
+
+  let newLI = $("<li>").text(`Title:${titleText} Rating: ${ratingNum}`);
+  newLI.append(deleteButton);
+
+  unorderList.append(newLI);
 }
+
+function deleteMovie(evt) {
+  let movieToDelete = $(evt.currentTarget.parentElement);
+
+  movieToDelete.remove();
+}
+
+//sorting
+//grab the UL jquery array object
+//convert the jQ object into an array
+//call sort on the array
+//delete the contents of the current UL
+//iterate over the array like object and append each element to the dom
