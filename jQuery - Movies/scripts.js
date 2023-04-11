747fbcd60a0ca1691bfe6973bc091dc889dd4ea1
Steven <stzheng716@gmail.com>
2023-04-10 17:26:35 -0700

    initial movie app

--- jQuery - Movies/scripts.js
+++ jQuery - Movies/scripts.js
@@ -1,1 +1,14 @@
-$('body').css('backgroundColor', 'red')
+//use eventpreventdefault in the event listener
+
+let unorderList = $('ul')
+
+$('form').on('submit', getValueAndappendDOM)
+
+function getValueAndappendDOM(evt){
+    evt.preventDefault()
+    const titleText = $('#title').val();
+    const ratingNum = $('#rating').val();
+
+    let newLI = $("<li>").text(`${titleText}: Rating ${ratingNum}`)
+    unorderList.append(newLI)
+}
